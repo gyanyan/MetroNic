1c784297abf3ced91d4608b579b62c36734b96b3
zltian <zltian@Ctrip.com>
2015-05-11 14:57:19 +0800

    Create MetroNicMvc Project.

---
+++ MetroNicMvc/App_Start/BundleConfig.cs
@@ -0,0 +1,36 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace MetroNicMvc
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/core/js").Include(
+                        "~/assets/plugins/jquery-1.10.2.min.js",
+                        "~/assets/plugins/jquery-migrate-1.2.1.min.js",
+                        "~/assets/plugins/jquery-ui/jquery-ui-1.10.3.custom.min.js",    //IMPORTANT! Load jquery-ui-1.10.3.custom.min.js before bootstrap.min.js to fix bootstrap tooltip conflict with jquery ui tooltip
+                        "~/assets/plugins/bootstrap/js/bootstrap.min.js",
+                        "~/assets/plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.min.js",
+                        "~/assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js",
+                        "~/assets/plugins/jquery.blockui.min.js",
+                        "~/assets/plugins/jquery.cokie.min.js",
+                        "~/assets/plugins/uniform/jquery.uniform.min.js"));
+
+            bundles.Add(new StyleBundle("~/global/css").Include(
+                "~/assets/plugins/font-awesome/css/font-awesome.min.css",
+                "~/assets/plugins/bootstrap/css/bootstrap.min.css",
+                "~/assets/plugins/uniform/css/uniform.default.css"));
+
+            bundles.Add(new StyleBundle("~/theme/css").Include(
+                "~/assets/css/style-metronic.css",
+                "~/assets/css/style.css",
+                "~/assets/css/style-responsive.css",
+                "~/assets/css/plugins.css",
+                "~/assets/css/themes/default.css",
+                "~/assets/css/custom.css"));
+        }
+    }
+}
---
+++ MetroNicMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MetroNicMvc.MvcApplication" Language="C#" %>
