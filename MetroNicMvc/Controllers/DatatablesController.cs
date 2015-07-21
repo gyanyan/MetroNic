957adfc6a54d9c764154a47f90d54444ab264815
zltian <zltian@Ctrip.com>
2015-07-21 13:57:27 +0800

    新增Datatables插件DEMO

---
+++ MetroNicMvc/Controllers/DatatablesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MetroNicMvc.Controllers
+{
+    public class DatatablesController : Controller
+    {
+        public ActionResult BasicTable()
+        {
+            return View();
+        }
+
+        public ActionResult DatatablesPlugin()
+        {
+            return View();
+        }
+    }
+}
